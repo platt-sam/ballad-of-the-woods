d94ff8a6a15762a2440ac2abde09a4f111b35e3f
Sam Platt <samplatt@sams-macbook.home>
2022-04-30 01:08:50 -0700

    templates

--- js/titleScreen.js
+++ js/titleScreen.js
@@ -1,18 +1,3 @@
-// Variables for all rooms
-
-var currentRoom = null;
-
-var roomExample = { // An object to represent a room
-  room_name : "example name", // the name of the room
-  
-  // Neighboring rooms
-  
-  roomUp : null,   // the room above/north/up
-  roomDown : null, // the room below/south/down
-  roomLeft : null, // the room to the left/west
-  roomRight : null // the room to the right/east
-};
-
 var h1TitleScreen = "<h1 id='titleScreen'>The Woods</h1>";
 var h2TitleScreen = "<h2 id='titleScreen'>Created by: Dean Wilson, Gianna McCardell, Sam Platt</h2>";
 var imgTitleScreen = "<img src='https://greekreporter.com/wp-content/uploads/2021/08/Sun-parthenon-credit-Mstyslav-Chernov-wikimedia-commons.jpg' style='border: solid 1px black; width: 40vw; filter: grayscale(100%);' alt='image for title screen' />";
--- js/topdown.js
+++ js/topdown.js
@@ -1,101 +1,78 @@
-let Room = class {
-    constructor(image, height, width, room_north, room_south, room_east, room_west, obstacle_ne, obstacle_nw, obstacle_se, obstacle_sw) {
-        this.image = image;
-        this.height = height;
-        this.width = width;
-        // neighboring rooms
-        this.room_north = room_north;
-        this.room_south = room_south;
-        this.room_east = room_east;
-        this.room_west = room_west;
-        // obstacles
-        this.obstacle_ne = obstacle_ne;
-        this.obstacle_nw = obstacle_nw;
-        this.obstacle_se = obstacle_se;
-        this.obstacle_sw = obstacle_sw;
-    }
-}
-let Obstacle = class {
-    constructor(image, height, width) {
-        this.image = image;
-        this.height = height;
-        this.width = width;
-    }
-};
-let Player = class {
-    constructor(image, height, width) {
-        this.image = image;
-        this.height = height;
-        this.width = width;
-    }
-}
 
-var p = new Player("media/lincoln.jpg", 0, 0);
+currentRoom = c; // Set Current Room at beginning of game
 
-var stoneObstacle = new Obstacle("", 80, 80);
+/* CREATE HTML ELEMENTS */
+    
+const body = document.getElementsByTagName("body");
+const main = document.createElement("main");
+const lincoln = document.createElement("img");
 
-var example = new Room("url('https://ak.picdn.net/shutterstock/videos/1017427615/thumb/11.jpg')", 400, 500);
+let ne_obstacle = document.createElement("img");
+let nw_obstacle = document.createElement("img");
+let se_obstacle = document.createElement("img");
+let sw_obstacle = document.createElement("img");
 
-example.obstacle_ne = stone;
+var mainLeftOffset = 0; // player's distance from the left border (in pixels)
+var mainTopOffset = 0; // player's distance from the top border (in pixels)
 
 function initialize() {
-    /* CREATE HTML ELEMENTS */
-    
-    const body = document.querySelector("body");
-    const main = document.createElement("main");
-    const player = document.createElement("img");
-    const stone = document.createElement("img");
 
-    /* SET ELEMENT STYLES */
-
-    var widthOfMain = 500; // width of the playable area (in pixels)
-    var heightOfMain = 400; // height of the playable area (in pixels)
-
-    var mainLeftOffset = 0; // player's distance from the left border (in pixels)
-    var mainTopOffset = 0; // player's distance from the top border (in pixels)
+    console.log("The current room is " + currentRoom.name);
 
     main.style.border = "solid 1px black";
-    main.style.height = example.height + "px";
-    main.style.width = example.width + "px";
+    main.style.height = room_height + "px";
+    main.style.width = room_width + "px";
     main.style.position = "fixed";
     main.style.marginLeft = mainLeftOffset;
     main.style.marginTop = mainTopOffset;
     main.style.padding = 0;
 
     // Background Image
-    main.style.backgroundImage = example.image;
+    main.style.backgroundImage = "url(" + currentRoom.image_src + ")";
     main.style.backgroundSize = "cover";
     main.style.backgroundRepeat = "no-repeat";
     main.style.padding = 0;
 
     // Player
-    player.src = player.image;
-    player.onload = function() {
+    lincoln.src = p1.source_filename;
+    lincoln.width = player_width;
+    lincoln.height = player_height;
+    lincoln.onload = function() {
         var w = 50;
         var h = Math.ceil((this.naturalHeight / this.naturalWidth * w) / 10) * 10;
-        console.log(w, h);
+        // console.log(w, h);
         this.style.width = w + "px";
         this.style.height = h + "px";
     }
-    player.style.position = "fixed";
-    player.style.marginLeft = 0;
-    player.style.marginTop = 0;
+    lincoln.style.position = "fixed";
+    lincoln.style.marginLeft = 0;
+    lincoln.style.marginTop = 0;
 
-    // Item in top right corner
-    stone.src = "https://pngimg.com/uploads/stone/stone_PNG13545.png";
-    stone.style.width = stone.width + "px";
-    stone.style.height = stone.height + "px";
-    stone.style.position = "fixed";
-    stone.style.marginLeft = example.width - stone.width + "px";
-    stone.style.marginTop = "0px";
-    stone.style.filter = "grayscale(100)";
+    // Northeast Obstacle
+    ne_obstacle.src = currentRoom.obstacle_ne.source_filename;
+    ne_obstacle.style.width = obstacle_width + "px";
+    ne_obstacle.style.height = obstacle_height + "px";
+    ne_obstacle.style.position = "fixed";
+    ne_obstacle.style.marginLeft = room_width - obstacle_width + "px";
+    ne_obstacle.style.marginTop = "0px";
+    ne_obstacle.style.filter = "grayscale(100)";
+    ne_obstacle.style.backgroundColor = "white";
+
+    // Northwest Obstacle
+
+    // Southeast Obstacle
+
+    // Southwest Obstacle
 
     /* SET THE INNER CONTENT OF THE ELEMENTS */
 
     /* LINK THE ELEMENTS TOGETHER */
-    body.append(main);
-    main.append(player);
-    main.append(stone);
+    body[0].append(main);
+    main.append(lincoln);
+    if (currentRoom.obstacle_ne) { main.append(ne_obstacle); }
+    if (currentRoom.obstacle_nw) { main.append(nw_obstacle); }
+    if (currentRoom.obstacle_se) { main.append(se_obstacle); }
+    if (currentRoom.obstacle_sw) { main.append(sw_obstacle); }
 
     /* ADD EVENT LISTENERS */
 
@@ -103,39 +80,57 @@ function initialize() {
         // console.log(`KeyboardEvent: ${event.key}`); // for debugging and testing purposes
         var key = event.key;
 
+        var northeast_corner_cond = null;
+        var northwest_corner_cond = null;
+        var southeast_corner_cond = null;
+        var southwest_corner_cond = null;
+
         var inc = 10; // How many pixels to move at a time
 
         if (key == 'w' || key == 'ArrowUp') { // Move Up
-            if (mainTopOffset > stone.height || 
-                (mainTopOffset > 0 && (mainLeftOffset + player.width) < (widthOfMain - stone.width))
-            ) {
+            
+            // Set corner conditions for going up
+            northeast_corner_cond = mainTopOffset > 0 && (mainLeftOffset + lincoln.width) < (room_width - ne_obstacle.width);
+            northwest_corner_cond = false; // TODO: Add logic for this
+            
+            if (mainTopOffset > ne_obstacle.height || northeast_corner_cond || northwest_corner_cond) {
+                
                 mainTopOffset -= inc;
-                player.style.marginTop = mainTopOffset + "px";
-            } else {
-                console.log("Reached topmost boundary of map");
-            }
+                lincoln.style.marginTop = mainTopOffset + "px";
+            
+            } else if (mainTopOffset == 0) { // if this is actually the top boundary of the map
+
+                if (currentRoom.room_north != null) { // if there is a room to the north
+                    
+                    console.log("Reached topmost boundary of map, moving to neighbor room to the north");
+                    currentRoom = currentRoom.room_north; // switch to that room
+                    initialize(); // re-initialize map
+
+                } else { console.log("Reached topmost boundary of map, but there is no neighbor to the north"); }
+            
+            } else { console.log("At corner case"); }
+        
         } else if (key == 's' || key == 'ArrowDown') { // Move Down
-            if (mainTopOffset + player.height < heightOfMain) {
+            if (mainTopOffset + lincoln.height < room_height) {
                 mainTopOffset += inc;
-                player.style.marginTop = mainTopOffset + "px";
+                lincoln.style.marginTop = mainTopOffset + "px";
             } else {
                 console.log("Reached bottommost boundary of map");
             }
-
         } else if (key == 'a' || key == 'A' || key == 'ArrowLeft') { // Move Left
             if (mainLeftOffset > 0) {
                 mainLeftOffset -= inc;
-                player.style.marginLeft = mainLeftOffset + "px";
+                lincoln.style.marginLeft = mainLeftOffset + "px";
             } else {
                 console.log("Reached leftmost boundary of map");
             }
 
         } else if (key == 'd' || key == 'ArrowRight') { // Move Right
-            if (mainLeftOffset + player.width < widthOfMain - stone.width ||
-                (mainLeftOffset + player.width < widthOfMain && (mainTopOffset > stone.height))
+            if (mainLeftOffset + lincoln.width < room_width - ne_obstacle.width ||
+                (mainLeftOffset + lincoln.width < room_width && (mainTopOffset > ne_obstacle.height))
             ) {
                 mainLeftOffset += inc;
-                player.style.marginLeft = mainLeftOffset + "px";
+                lincoln.style.marginLeft = mainLeftOffset + "px";
             } else {
                 console.log("Reached rightmost boundary of map");
             }
